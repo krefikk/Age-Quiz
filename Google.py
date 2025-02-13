9b77a19470d257f36f169719ed2e362e2749001d
krefikk <ahmetrefikks@gmail.com>
2025-02-13 19:37:07 +0300

    flow fixes with converting pickle to txt

--- Google.py
+++ Google.py
@@ -5,20 +5,36 @@ from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from google.auth.transport.requests import Request
+import base64
 
+def decode_token():
+    # Path to the encoded secret file in Render
+    encoded_file_path = '/etc/secrets/encoded_token.txt'
+    pickle_file_path = 'token_drive_v3.pickle'
 
-def Create_Service(client_secret_file, api_name, api_version, *scopes):
-    print(client_secret_file, api_name, api_version, scopes, sep='-')
-    CLIENT_SECRET_FILE = client_secret_file
-    API_SERVICE_NAME = api_name
-    API_VERSION = api_version
-    SCOPES = [scope for scope in scopes[0]]
-    print(SCOPES)
+    if os.path.exists(encoded_file_path):
+        # Read the encoded Base64 token from the secret file
+        with open(encoded_file_path, 'r') as encoded_file:
+            encoded_token = encoded_file.read()
 
+        # Decode the Base64 string
+        decoded_token = base64.b64decode(encoded_token)
+
+        # Write the decoded data to the pickle file
+        with open(pickle_file_path, 'wb') as token_file:
+            token_file.write(decoded_token)
+
+        print(f"Decoded token saved to {pickle_file_path}")
+    else:
+        print("Encoded token file not found!")
+
+# Call this function at the start of your script
+decode_token()
+
+# Then proceed with the usual flow to use the pickle file
+def Create_Service(client_secret_file, api_name, api_version, scopes):
     cred = None
-
-    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
-    # print(pickle_file)
+    pickle_file = f'token_{api_name}_{api_version}.pickle'
 
     if os.path.exists(pickle_file):
         with open(pickle_file, 'rb') as token:
@@ -28,22 +44,18 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
         if cred and cred.expired and cred.refresh_token:
             cred.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-            # cred = flow.run_local_server(port=8080)
-            # cred = flow.run_console()
+            flow = InstalledAppFlow.from_client_secrets_file(client_secret_file, scopes)
             cred = flow.run_local_server(port=8080, open_browser=False)
-            # cred = flow.run_local_server()
 
         with open(pickle_file, 'wb') as token:
             pickle.dump(cred, token)
 
     try:
-        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
-        print(API_SERVICE_NAME, 'service created successfully')
+        service = build(api_name, api_version, credentials=cred)
+        print(f'{api_name} service created successfully')
         return service
     except Exception as e:
-        print('Unable to connect.')
-        print(e)
+        print(f'Failed to create {api_name} service: {e}')
         return None
 
 def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
